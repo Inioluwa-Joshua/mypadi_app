d50de8dda24dce10fe9641242cde0c889d4b487b
Inioluwa-Joshua <joshuainioluwa15@gmail.com>
2024-05-17 21:26:50 +0100

    update 17/5/24

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-K6PXKLVXMT"
+      />
+      <script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K6PXKLVXMT');
+        `}
+      </script>
+      </head>
       <body className={inter.className}>
           {children}
       </body>
